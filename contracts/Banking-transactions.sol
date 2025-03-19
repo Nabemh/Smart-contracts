2503f60bb9b512f6858f5069424662af5c228d44
Nabemh <nhanior@gmail.com>
2025-03-19 10:34:24 +0100

    more functionality

--- contracts/Banking-transactions.sol
+++ contracts/Banking-transactions.sol
@@ -22,6 +22,21 @@ contract Transactions{
         _;
     }
 
-    
+    function deposit(uint i, uint amount) public OnlyOwner{
+       accounts[msg.sender][i].balance += amount;
+    }
+
+    function withdraw(uint i, uint amount) public OnlyOwner{
+        require(accounts[msg.sender][i].balance >= amount, "Insuffient funds");
+        accounts[msg.sender][i].balance -= amount;
+    }
+
+    function transfer(uint i, address receiver, uint amount) public OnlyOwner{
+        
+    }
+
+    function getBalance(uint i) public view returns(uint){
+        return accounts[msg.sender][i].balance;
+    }
 
 }
