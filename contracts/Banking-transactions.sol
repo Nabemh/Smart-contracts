0818b106d550222ecea112be2fed56e157564b25
Nabemh <nhanior@gmail.com>
2025-03-19 10:07:54 +0100

    modifiers

--- contracts/Banking-transactions.sol
+++ contracts/Banking-transactions.sol
@@ -7,9 +7,21 @@ contract Transactions{
     struct Account{
         uint balance;
         uint timestamp;
-        address sender;
+        address id;
     }
 
     mapping (address => Account[]) public accounts;
+    address public owner;
+
+    constructor (){
+        owner == msg.sender;
+    }
+
+    modifier OnlyOwner(){
+        require(msg.sender == owner, "Not owner");
+        _;
+    }
+
+    
 
 }
