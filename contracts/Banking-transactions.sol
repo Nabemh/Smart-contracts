c8697e796f364eb19e1f07796e0bcbefe2444b79
Nabemh <nhanior@gmail.com>
2025-03-19 09:56:43 +0100

    mappings

--- contracts/Banking-transactions.sol
+++ contracts/Banking-transactions.sol
@@ -3,5 +3,13 @@
 pragma solidity ^0.8.0;
 
 contract Transactions{
-    
+
+    struct Account{
+        uint balance;
+        uint timestamp;
+        address sender;
+    }
+
+    mapping (address => Account[]) public accounts;
+
 }
