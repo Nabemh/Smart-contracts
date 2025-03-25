45b6b749ac8d4b90f3defed51caa4de0b66cef23
Nabemh <nhanior@gmail.com>
2025-03-25 20:32:18 +0100

    interface

--- contracts/Banking-transactions.sol
+++ contracts/Banking-transactions.sol
@@ -2,6 +2,15 @@
 
 pragma solidity ^0.8.0;
 
+interface  IProfile {
+        struct CustomerProfile {
+        address userAddress; 
+        string username; 
+        uint balance;
+        uint creationTimestamp;
+    } 
+}
+
 contract Transactions{
 
     struct Account{
